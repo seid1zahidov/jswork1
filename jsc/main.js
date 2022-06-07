dd40603ae4b6a70dc8dd90b977639a6e94873b81
Seid <101628221+seid1zahidov@users.noreply.github.com>
2022-06-07 23:56:31 +0400

    jswokr1

---
+++ jsc/main.js
@@ -0,0 +1,17 @@
+var hour  = new Date().getHours();
+if(hour > 20 && hour < 22){
+    document.body.style.backgroundColor = 'red'
+}
+else if (hour >22 && hour > 23) {
+    document.body.style.backgroundColor = 'orange'
+}
+else if (hour =>23 && hour > 24) {
+    document.body.style.backgroundColor = 'purple'
+}
+
+
+
+
+console.log(Math.pow(2,2))
+
+
